58fa0283e1a9bf38ab844c8674e85640fd0246c1
ujunwa <ujmarylee@gmail.com>
2023-05-17 22:28:20 -0700

    initial

--- Api_app/serializers.py
+++ Api_app/serializers.py
@@ -12,7 +12,7 @@ class PostSerializer(serializers.ModelSerializer):
 
 
 
-# class CommentSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Comment
-#         fields = "__all__"
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = "__all__"
